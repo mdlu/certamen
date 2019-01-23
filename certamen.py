e068149eb420e8a281f04179cc1d92369e236166
mdlu <mdlu841@gmail.com>
2019-01-22 21:35:58 -0600

    improves single-player experience
    all implemented for single-player except for physical buzzing

--- certamen.py
+++ certamen.py
@@ -3,6 +3,23 @@ from extract_round import extract_round
 from reader import Reader
 
 
+# engine = pyttsx3.init()
+# def onStart(name):
+#    print('starting', name)
+# def onWord(name, location, length):
+#    print('word', name, location, length)
+# def onEnd(name, completed):
+#    print('finishing', name, completed)
+#    if name == 'fox':
+#       engine.say('What a lazy dog!', 'dog')
+#    elif name == 'dog':
+#       engine.endLoop()
+# engine = pyttsx3.init()
+# # engine.connect('started-utterance', onStart)
+# # engine.connect('started-word', onWord)
+# # engine.connect('finished-utterance', onEnd)
+# engine.say('The quick brown fox jumped over the lazy dog.', 'fox')
+# engine.startLoop()
 
 if __name__ == '__main__':
     
@@ -12,8 +29,16 @@ if __name__ == '__main__':
     specs = ['Finals', 15]
     r = Reader(yaleadv18)
 
-    r.read_round('Round 1')
+    r.read_round('Round 3')
 
+    # e = pyttsx3.init()
+    
+    # e.say('hello')
+    # e.startLoop()
+    # #e.iterate()
+    # e.stop()
+    # e = pyttsx3.init()
+    # e.say('goodbye')
 
     # zira = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
     # engine.setProperty('voice', zira)
--- reader.py
+++ reader.py
@@ -1,12 +1,16 @@
 import pyttsx3
+from queue import Queue
 
 class Reader:
     """ Creates a certamen moderator. """
 
     def __init__(self, rounds):
         self.engine = pyttsx3.init()
-        self.engine.setProperty('rate', 140)
+        self.engine.setProperty('rate', 150)
+        self.engine2 = pyttsx3.init()
         self.rounds = rounds
+        self.score = 0
+
     
     def read(self, specs):
         """ The computer reads the requested section of text from the rounds the reader holds.
@@ -17,6 +21,7 @@ class Reader:
             Returns:
             None
         """
+        
         t = ''
 
         if len(specs) == 2: # this is when we're reading the starting info for the round
@@ -27,9 +32,9 @@ class Reader:
                 t = 'Tossup'
 
         self.engine.say(t + text.lower()) # changing to lowercase avoids issues where uppercase is read as an abbreviation
-        self.engine.runAndWait()
-
-        return None
+        
+        #self.engine.startLoop()
+     
 
 
     def tossup(self, specs):
@@ -42,36 +47,104 @@ class Reader:
             None
         """
         s = [specs[0], specs[1], 'Tossup', 'Q']
-        try:
-            self.read(s)
-            self.engine.say('Repeating:')
-            self.read(s)
-            self.engine.say('I will have to call time.')
-            self.engine.say('The correct answer is:')
-            s[3] = 'A'
-            self.read(s)
+        tossup_score = 0
 
-        except KeyboardInterrupt:
-            self.engine.endLoop()
-            print('interrupted! how rude')
-            self.engine.stop()
-            # engine.startLoop(False)
-            # engine.say('Answer please.')
-
-            # engine.iterate()
-            # engine.endLoop()
-
-            # engine.say('The correct answer is:')
-            # s[3] = 'A'
-            # read(engine, rounds, s)
-            # engine.say('Was that your answer?')
-            # engine.runAndWait()
+        self.read(s)
+        self.engine.runAndWait()
         
-        return None
+        correct = self.answer(s)
+        
+        if correct:
+            tossup_score += 10
+            s[2] = 'Bonus 1'
+            s[3] = 'Q'
+            self.read(s)
+            self.engine.runAndWait()
+            correct1 = self.answer(s)
+            if correct1:
+                tossup_score += 5
+            
+            s[2] = 'Bonus 2'
+            s[3] = 'Q'
+            self.read(s)
+            self.engine.runAndWait()
+            correct2 = self.answer(s)
+            if correct2:
+                tossup_score += 5
+        
+        self.score += tossup_score
+
+        self.engine.say("So that's %d points to you on tossup %d." % (tossup_score, specs[1]))
+        self.engine.runAndWait()
+
+        # try: 
+        #     self.read(s)
+        #     self.engine.say('Repeating:')
+        #     self.read(s)
+        #     self.engine.say('I will have to call time.')
+        #     self.engine.say('The correct answer is:')
+        #     s[3] = 'A'
+        #     self.read(s)
+
+        #     #self.engine.runAndWait()
+        #     self.engine.startLoop()
+        #     #self.engine.endLoop()
+            
+        #     return False
+
+        # except KeyboardInterrupt:
+        #     self.engine.endLoop()
+        #     # self.engine.say('testing')
+        #     # self.engine.startLoop()
+        #     # self.engine.endLoop()
+        #     #self.engine.stop()
+            
+        #     #self.tossup_answer(s)
+        #     #self.engine.endLoop()
+
+        #     # ADD implementation of boni if the tossup is answered correctly
+            
+        #     return True
     
+    def answer(self, s):
+        
+        self.engine.say('What is your answer?')
+        self.engine.runAndWait()
+        x = input('Answer: ')
+        
+        self.engine.say('The correct answer was:')
+        self.engine.runAndWait()
+
+        s[3] = 'A'
+        self.read(s)
+        self.engine.runAndWait()
+
+        self.engine.say('Was that your answer?')
+        self.engine.runAndWait()
+
+        y = input("Type 'y' or 'n': ")
+        if y == 'y':
+            self.engine.say('Great!')
+            self.engine.runAndWait()
+            
+            return True
+        else:
+            self.engine.say('Sorry.')
+            self.engine.runAndWait()
+
+            return False
+
+
     def read_round(self, round):
-        self.read([round, 'Info'])
+        #self.read([round, 'Info'])
 
         for i in range(1, 21):
             s = [round, i]
             self.tossup(s)
+        
+        self.engine.say('Your final score is:', self.score)
+        self.engine.runAndWait()
+
+            
+        #self.engine.startLoop()
+        
